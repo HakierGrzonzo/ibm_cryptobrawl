d12ea8e6dab5f3977a0e98b78807500583446015
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-10-17 20:45:31 +0200

    Tym razem kamilku hasła nie dostaniesz XD

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,9 @@ import datetime
 import threading
 import json
 from time import sleep
+import urllib3
+
+urllib3.disable_warnings()
 
 class API:
     def __init__(self, username, password):
@@ -14,55 +17,10 @@ class API:
         })
         self._username = username
         self._password = password
-        self.pre_rate_change_hooks = []
-        self.after_rate_change_hooks = []
-        self.realprice = None
-        # set to False to stop threads
-        self._allow_threads = True
-        self.rates = None
         self.reconnect()
-        self._rates_thread = threading.Thread(target=self._rates_worker)
-        self._rates_thread.start()
-       # self.pre_rate_change_hooks.append(self.test_worker)
-        self.after_rate_change_hooks.append(self.earn_from_rounding)
-        #Diffrent after_rate_change_hooksbetween actually value and calculated by IBM
-        self.roundedDiffrenceETH = 0.0
-        self.roundedDiffrenceBTC = 0.0
-
-        #print(self.get_rates())
-        #print(self.get_team())
-        #transaction = self.transaction("usd", 69, "ETH")
-        #print(transaction)
-        #breakpoint()
-        #print(self.confirm_transaction(transaction["entity"]["transactionID"]))
-
-    def test_worker(self):
-        print(self.realprice['bitcoin']['usd'], self.rates[0]['rate'])
-
-    def earn_from_rounding(self):
-        self.roundedDiffrenceETH = round(self.rates[3]["rate"], 6) * self.rates[1]["rate"]
-        self.roundedDiffrenceBTC = round(self.rates[2]["rate"], 6) * self.rates[0]["rate"]
-        how_much_to_buy = 213700
-        print(self.rates)
-        print(self.roundedDiffrenceETH)
-        if(self.roundedDiffrenceETH>1.0008):
-            print("start ETH treade because rate is: ", self.roundedDiffrenceETH)
-            transaction = self.transaction("usd", how_much_to_buy, "ETH")
-            self.confirm_transaction(transaction["entity"]["transactionID"])
-            sleep(7)
-            transaction = self.transaction("ETH", round(round(self.rates[3]["rate"], 6) * how_much_to_buy, 6), "usd")
-            self.confirm_transaction(transaction["entity"]["transactionID"])
-        if(self.roundedDiffrenceBTC>1.0008):
-            print("start BTC treade because rate is: ", self.roundedDiffrenceBTC)
-            transaction = self.transaction("usd", how_much_to_buy, "BTC")
-            self.confirm_transaction(transaction["entity"]["transactionID"])
-            sleep(7)
-            transaction = self.transaction("BTC", round(round(self.rates[2]["rate"], 6) * how_much_to_buy, 6), "usd")
-            self.confirm_transaction(transaction["entity"]["transactionID"])
-
-
 
     def reconnect(self):
+        """
         try:
             with open(".cookie", "r") as f:
                 cookie = json.load(f)
@@ -70,9 +28,10 @@ class API:
                     self.session.cookies[k] = v
                 if self.session.get("https://platform.cryptobrawl.pl/api/rates", verify=False).status_code == 200:
                     print("Got cached!")
-                    #return
+                    return
         except:
             pass
+        """
         print("Reconnecting!")
         options = webdriver.FirefoxOptions()
         options.headless = True
@@ -112,58 +71,16 @@ class API:
                     }
                 },
                 verify=False)
-        print(request.text)
         return request.json()
 
-    def confirm_transaction(self, transaction_id: str):
+    def confirm_transaction(self, transaction_id):
+        if not isinstance(transaction_id, str):
+            transaction_id = transaction_id['entity']['transactionID']
         return self.session.post(
                 "https://platform.cryptobrawl.pl/api/transactions/{}".format(transaction_id),
                 verify=False
             )
 
-    def run_hooks(self, hooks):
-        #self.realprice = cg.get_price(ids="bitcoin", vs_currencies="usd")
-        for hook in hooks:
-            hook()
-
-    def _rates_worker(self):
-        while self._allow_threads:
-            try:
-                print("Getting rates!")
-                rates = self.get_rates()
-                self.rates = rates["entity"]
-                update_again_at = datetime.datetime.fromtimestamp(rates["metadata"]["expiresAt"])
-                if not self._allow_threads:
-                    break
-                print("after rate hooks", flush=True)
-                hook_thread = threading.Thread(target=self.run_hooks, args=(self.after_rate_change_hooks,))
-                hook_thread.start()
-                time_left = update_again_at - datetime.datetime.now()
-                sleep(time_left.total_seconds() / 2 + 1)
-
-                hook_thread.join()
-                if not self._allow_threads:
-                    break
-                print("pre rate hooks", flush=True)
-                hook_thread = threading.Thread(target=self.run_hooks, args=(self.pre_rate_change_hooks,))
-                hook_thread.start()
-                time_left = update_again_at - datetime.datetime.now()
-                sleep(time_left.total_seconds() + 1)
-                hook_thread.join()
-            except Exception as e:
-                print("_rates_worker:", e, flush=True)
-                sleep(10)
-
-    def __del__(self):
-        print("Killing threads")
-        self._allow_threads = False
-        self._rates_thread.join()
-
-
-
-
-
-
-
-
+    def get_update_datetime(self, rates):
+        return datetime.datetime.fromtimestamp(rates['metadata']['expiresAt'])
 
