c7560782a3242523dd35e5bcf5b33fe48cab3b21
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-10-24 19:13:40 +0200

    Pre-test

--- api/__main__.py
+++ api/__main__.py
@@ -10,14 +10,27 @@ WARNING_TIME = 600 # ile sekund przed handlem mam wysłać przypomnienie
 
 logfile = open("logfile.tsv", "w+")
 
+iteration = 0
 while True:
     api = API("grzekop680@student.polsl.pl", "tu_wpisz_swoje_hasło_xd")
-    bought_eth = 0
     bought_ratio_eth = 0
-    bought_btc = 0
     bought_ratio_btc = 0
     team = api.get_team()
     usd = round(float(team['entity']['balance']['currencies']['usd']) * .95)
+    bought_btc = float(team['entity']['balance']['currencies']['btc'])
+    if bought_btc > 0:
+        transaction = api.transaction('btc', bought_btc, 'usd')
+        if api.confirm_transaction(transaction).status_code == 200:
+            print("Sold BTC")
+            bought_btc = 0
+            usd += float(transaction['entity']['boughtAmount'])
+    bought_eth = float(team['entity']['balance']['currencies']['eth'])
+    if bought_eth > 0:
+        transaction = api.transaction('eth', bought_eth, 'usd')
+        if api.confirm_transaction(transaction).status_code == 200:
+            print("Sold ETH")
+            bought_eth = 0
+            usd += float(transaction['entity']['boughtAmount'])
     #usd = 10000
     started_usd = usd
     print("Beggining trade with", usd, "USD!", flush=True)
@@ -29,7 +42,6 @@ while True:
         time.sleep(max(2, time_to_next_update.total_seconds()))
         rates = api.get_rates()
     print("Synchronizacja z patolą osiągnięta, zaczynam ojebywać IBM", flush=True)
-    iteration = 0
     while True:
         print(iteration, flush=True)
         iteration += 1
@@ -38,9 +50,13 @@ while True:
             bitcoin_ibm = rates['entity'][0]["rate"]
             ethereum_ibm = rates['entity'][1]['rate']
             if (
-                    bought_btc > 0 and 
-                    abs(bitcoin_ibm - bought_ratio_btc) > 1 and
-                    abs(bitcoin_ibm - bitcoin['usd']) < 1
+                    bought_btc > 0 and (
+                        (
+                            abs(bitcoin_ibm - bought_ratio_btc) > 1 and
+                            abs(bitcoin_ibm - bitcoin['usd']) < 1
+                        ) or
+                        bitcoin['usd'] < bought_ratio_btc
+                    )
                 ):
                 transaction = api.transaction('btc', bought_btc, 'usd')
                 if api.confirm_transaction(transaction).status_code == 200:
@@ -49,9 +65,13 @@ while True:
                     usd += float(transaction['entity']['boughtAmount'])
                     print("PROFIT:", usd - started_usd)
             if (
-                    bought_eth > 0 and 
-                    abs(ethereum_ibm - bought_ratio_eth) > .5 and
-                    abs(ethereum_ibm - ethereum['usd']) < .5
+                    bought_eth > 0 and (
+                        (
+                            abs(ethereum_ibm - bought_ratio_eth) > .5 and
+                            abs(ethereum_ibm - ethereum['usd']) < .5
+                        ) or 
+                        ethereum['usd'] < bought_ratio_eth
+                    )
                 ):
                 transaction = api.transaction('ETH', bought_eth, 'usd')
                 if api.confirm_transaction(transaction).status_code == 200:
@@ -113,15 +133,7 @@ while True:
             rates = api.get_rates()
         except Exception as e:
             print("EXCEPTION:", e)
-            time.sleep(120)
-            api.reconnect()
-            rates = api.get_rates()
-            time_to_next_update = api.get_update_datetime(rates) - datetime.datetime.now()
-            if time_to_next_update.total_seconds() < 30:
-                # skip first period if we are late
-                time.sleep(max(2, time_to_next_update.total_seconds()))
-                rates = api.get_rates()
-            print("Got back in action!", flush=True)
+            break
     print("Kończe trejdowanie", flush=True)
     time_to_sleep = datetime.timedelta(minutes=random.randint(1, 19))
     time_to_wake_up = datetime.datetime.now() + time_to_sleep
